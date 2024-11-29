d9f620e3498d5c5cc2aa6274621d5f51e0ba0129
Sofia W <sofiawberg@live.se>
2024-11-29 08:05:33 +0100

    changed link in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 		<div className='navbar'>
 			<nav>
 				<ul className='navbar__list'>
-					<li><Link to="/">Hem</Link></li>
+					<li><Link to="/">QTCC</Link></li>
 					<li><Link to="/calendar">Julkalender</Link></li>
 					<li><Link to="/rhymes">Rim</Link></li>
 					<li><Link to="/score">Poängställning</Link></li>

db1e6d9f23e0480b304d3b64a0d3c361baf62b45
Sofia W <sofiawberg@live.se>
2024-12-04 08:02:07 +0100

    added bounus point

--- src/pages/BonusPoints.jsx
+++ src/pages/BonusPoints.jsx
@@ -6,11 +6,17 @@ export const BonusPoints = () => {
     <div className="bonus-points pink-background">
       <h2>Bonuspoäng</h2>
       <ul>
-        <li>✨<strong>Grattis</strong> till 20 poäng för flest antal låtar i jullistan: <strong>Julstjärnorna </strong>✨</li>
-        <ul>
-          <li>⛄️ Snögubbarna: <strong>52 </strong>låtar</li>
-          <li>⭐️ Julstjärnorna: <strong>63</strong> låtar</li>
-        </ul>
+        <li>✨<strong>Grattis</strong> till 20 poäng för flest antal låtar i jullistan: <strong>Julstjärnorna </strong>✨
+          <ul>
+            <li>⛄️ Snögubbarna: <strong>52 </strong>låtar</li>
+            <li>⭐️ Julstjärnorna: <strong>63</strong> låtar</li>
+          </ul>
+        </li>
+        <li>✨<strong>Grattis</strong> till 20 poäng för minihackning: <strong>Julstjärnorna </strong>✨
+          <ul>
+            <li>⭐️ Jonathan: lyckades hitta innehåll på sidan för en kommande tävling </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );

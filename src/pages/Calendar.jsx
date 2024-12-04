9a46225f4f9b9c9742f3ed92014d0aa8365edeb1
Sofia W <sofiawberg@live.se>
2024-12-04 08:53:20 +0100

    removed 'imorgon' from door 5 and 6

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -95,7 +95,7 @@ const fetchDoorStatus = async (id) => {
             <div className='calendar__item--back' style={{backgroundImage: `url(${backgroundImage})`}}>
               <div className='calendar__item__back-content'>
                 <p>Idag: {apiData[event.id] ? apiData[event.id].content.now : ''}</p>
-                {!event.christmas && (
+                {!event.christmas || event.id !== 5 || event.id !== 6 && (
                   <p>Imorgon: {apiData[event.id] ? apiData[event.id].content.tomorrow : ''}</p>
                 )} 
               </div>
